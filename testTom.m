e7fee6445dbb27a151e77e97aa4cd7e14760a1c6
Tom Strube <tom.strube@stud.uni-due.de>
2020-04-23 18:29:52 +0200

    testCommitForGITsetup

---
+++ testTom.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = testTom(inputArg1,inputArg2)
+%TESTTOM Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
